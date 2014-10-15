2af1490e3312ab57484a6bafdf9a1eb168c7c9fc
root <root@pi2.(none)>
2014-10-15 12:43:25 +0000

    written

--- web/index.php
+++ web/index.php
@@ -1,19 +1,39 @@
 <?php
+require('settings.php');
 
-$db = new Sqlite3('../databas.sqlite');
+try {
+$db = new PDO('mysql:host=localhost;dbname=stackomat', 'stackomat', $password);
+
 if ($db) {
-	if (isset($update)) {
-	} else {
-		$s = $db -> prepare('SELECT id,name,cost FROM products;');
-		$data = "";
-		while (($row = $s -> fetchArray())) {
-			$data .= '<tr>';
-			$data .= '<td> <input type="text" name="id" value="'.$row['id'].'"></td>';
-			$data .= '<td> <input type="text" name="name" value="'.$row['name'].'"></td>';
-			$data .= '<td> <input type="text" name="cost" value="'.$row['cost'].'"></td>';
-			$data .= '</tr>';
+	if (isset($_GET['update'])) {
+		$id = $_POST['id'];
+		$name = $_POST['name'];
+		$cost = $_POST['cost'];
+		$s = $db -> prepare('INSERT INTO products (id, name, cost) VALUES (:id, :name, :cost);');
+		$s -> bindParam(':id', $id);
+		$s -> bindParam(':name', $name);
+		$s -> bindParam(':cost', $cost);
+		if ($s -> execute()) {
+			$added = true;
+		} else {
+			$added = false;
+			$msg = $db -> lastErrorMsg();
 		}
 	}
+
+	$s = $db -> prepare('SELECT id,name,cost FROM products;');
+	$data = "";
+	$s -> execute();
+	foreach ($s -> fetchAll() as $row) {
+		$data .= '<tr>';
+		$data .= '<td> <input type="text" name="id" value="'.$row['id'].'"></td>';
+		$data .= '<td> <input type="text" name="name" value="'.$row['name'].'"></td>';
+		$data .= '<td> <input type="text" name="cost" value="'.$row['cost'].'"></td>';
+		$data .= '</tr>';
+	}
+}
+} catch (PDOException $e) {
+	$msg = $e -> getMessage();
 }
 
 ?>
@@ -26,7 +46,15 @@ if ($db) {
 	<meta charset='utf-8'>
 </head>
 <body>
-	<form action="index.php" method="post">
+	<?php 
+if (isset($added)) {
+if ($added) echo 'Produkten lades till.<br/>';
+else echo "Produkten kunde inte läggas till.<br/>";
+} 
+echo $msg;
+?>
+
+	<form action="index.php?update=true" method="post">
 	<table>
 		<tr>
 			<td>Produktens streckckod</td>
@@ -41,13 +69,15 @@ if ($db) {
 			<td><input type="text" name="cost"></td>
 		</tr>
 		<tr>
-			<td>Lösenord</td>
-			<td><input type="password" name="password"></td>
+			<td colspan="2">
+				<input type="submit" value="Lägg till">
+			</td>
 		</tr>
 	</table>
 
 	</form>
 
+	<?php if (!isset($data)) echo 'Kunde inte hämta data från databasen.';?>
 
 	<table><?php echo $data; ?></table>
 </body>
