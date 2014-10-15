baefc7d00ec74cc9c30c14e6183603edfb656492
Fabian Ström <fs@fabianstrom.se>
2014-10-15 22:45:26 +0200

    comments, update: not null on products.name

--- schema.sql
+++ schema.sql
@@ -1,37 +1,47 @@
 CREATE TABLE users (
+	-- the id/barcode of a user
 	id VARCHAR(255) 
 	   PRIMARY KEY 
 	   NOT NULL 
 	   UNIQUE,
 
+	-- the user's name, can be null
 	name TEXT,
 
+	-- the user's balance, must not be null
 	balance INTEGER 
 	        NOT NULL 
 	         DEFAULT 0
 );
 
 CREATE TABLE products (
+	-- the barcode of the product
 	id VARCHAR(255) 
 	   PRIMARY KEY 
 	   NOT NULL 
 	   UNIQUE,
 
-	name TEXT,
+	-- the name of a product, must be set
+	name TEXT
+	     NOT NULL,
 
+	-- the cost of a product, must be set
 	cost INTEGER 
 	     NOT NULL
 );
 
 CREATE TABLE lastPurchase (
+	-- the id if the user who did this purchase
 	id VARCHAR(255) 
 	   PRIMARY KEY 
 	   NOT NULL 
 	   UNIQUE 
 	   REFERENCES users (id),
 
+	-- the amount the user purchased for
 	amount INTEGER,
 
+	-- one should not be able to loose balance by undoing a purchase
 	CHECK (amount >= 0)
 );
 
