0596728de5d33b76082b80c31f50af6124ca4deb
Fabian Ström <fs@fabianstrom.se>
2014-11-13 19:42:35 +0100

    added calories

--- web/index.php
+++ web/index.php
@@ -5,16 +5,19 @@ function getInput() {
 	$id = $_POST['id'];
 	$name = $_POST['name'];
 	$cost = $_POST['cost'];
+	$calories = $_POST['calories'];
 	if (isset($_POST['action'])) $action = $_POST['action'];
 	if (!preg_match('/^[0-9]+$/', $id)) die();
 	if (!is_string($name)) die();
 	if (!preg_match('/^[0-9]+$/', $cost)) die();
+	if (!preg_match('/^[0-9]+$/', $calories)) die();
 	if (!(!isset($action) || $action == 'Uppdatera' || $action == 'Ta bort')) die();
 	return array(
 		'id' => $id, 
 		'name' => $name, 
 		'cost' => $cost,
-		'action' => $action
+		'action' => $action,
+		'calories' => $calories
 	);
 }
 
@@ -23,11 +26,13 @@ function handleAdd($db) {
 	$id = $input['id'];
 	$name = $input['name'];
 	$cost = $input['cost'];
+	$calories = $input['calories'];
 
-	$s = $db -> prepare('INSERT INTO products (id, name, cost) VALUES (:id, :name, :cost);');
+	$s = $db -> prepare('INSERT INTO products (id, name, cost, calories) VALUES (:id, :name, :cost, :calories);');
 	$s -> bindParam(':id', $id);
 	$s -> bindParam(':name', $name);
 	$s -> bindParam(':cost', $cost);
+	$s -> bindParam(':calories', $calories);
 	return  ($s -> execute());
 }
 
@@ -37,11 +42,12 @@ function handleUpdate($db) {
 	$name = $input['name'];
 	$cost = $input['cost'];
 	$action = $input['action'];
+	$calories = $input['calories'];
 
 	$result = false;
 	if ($action == 'Uppdatera') {
-		$s = $db -> prepare('UPDATE products SET name=?, cost=? WHERE id=?;');
-		$result = $s -> execute(array($name, $cost, $id));
+		$s = $db -> prepare('UPDATE products SET name=?, cost=?, calories=? WHERE id=?;');
+		$result = $s -> execute(array($name, $cost, $id, $calories));
 	} else if ($action == 'Ta bort') {
 		$s = $db -> prepare('DELETE FROM products WHERE id=?;');
 		$result = $s -> execute(array($id));
@@ -51,7 +57,7 @@ function handleUpdate($db) {
 }
 
 function listProducts($db) {
-	$s = $db -> prepare('SELECT id,name,cost FROM products ORDER BY name,cost;');
+	$s = $db -> prepare('SELECT id,name,cost,calories FROM products ORDER BY name,cost;');
 	$s -> execute();
 	return $s -> fetchAll();
 }
@@ -73,6 +79,7 @@ try {
 		$data .= '<td><input disabled="disabled" type="text" name="id" value="'.htmlentities($row['id']).'"></td>';
 		$data .= '<td><input type="text" name="name" value="'.htmlentities($row['name']).'"></td>';
 		$data .= '<td><input type="text" name="cost" value="'.htmlentities($row['cost']).'"></td>';
+		$data .= '<td><input type="text" name="calories" value="'.htmlentities($row['calories']).'"></td>';
 		$data .= '<td><input type="submit" name="action" value="Uppdatera"></td>';
 		$data .= '<td><input type="submit" name="action" value="Ta bort"></td>';
 		$data .= '</form>';
@@ -136,6 +143,7 @@ try {
 				<th>Streckkod</th>
 				<th>Namn</th>
 				<th>Kostnad</th>
+				<th>Kalorier</th>
 				<th>Åtgärder</th>
 			</tr>
 		</thead>
